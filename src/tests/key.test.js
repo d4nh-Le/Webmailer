4cb9a619a2907f62d215dc49210757a51381349b
Danh Le <danhle002@gmail.com>
2024-05-07 03:15:32 -0600

    key.test.js - added unit test for key.utilities methods: generateKey, checkValidKey and getKeyInfo

--- src/tests/key.test.js
+++ src/tests/key.test.js
@@ -1,8 +1,40 @@
 const keyModule = require('../utilities/key.utility');
 
+describe('Key.utility - generateKey()', () => {
 
+    it('Check key is not null functionality', () => {
+        const key = keyModule.generateKey();
+        expect(key).not.toBeNull();
+    });
+
+    it('Check key is string functionality', () => {
+        const key = keyModule.generateKey();
+        expect(typeof key).toBe('string');
+    });
+
+    it('Check generated key length functionality', () => {
+        const key = keyModule.generateKey();
+        expect(key.length).toBe(30);
+    });
+
+    it('Check generated unique key functionality', () => {
+        const key1 = keyModule.generateKey();
+        const key2 = keyModule.generateKey();
+        expect(key1).not.toBe(key2);
+      });
+});
 
 describe('Key.utility - checkValidKey()', () => {
+    it('Check undefined key functionality', () => {
+        const key = undefined;
+        expect(keyModule.checkValidKey(key)).toBe(false);
+    });
+
+    it('Check null key functionality', () => {
+        const key = null;
+        expect(keyModule.checkValidKey(key)).toBe(false);
+    });
+
     it('Check valid key functionality', () => {
         const key = 'webmailer_generatedtestkey0001';
         expect(keyModule.checkValidKey(key)).toBe(true);
@@ -20,7 +52,7 @@ describe('Key.utility - checkValidKey()', () => {
 });
 
 describe('Key.utility - getKeyInfo()', () => {
-    it('Key.utility - check return correct key_info functionality', async () => {
+    it('Check return correct key_info functionality', async () => {
         const keyInfo = await keyModule.getKeyInfo('webmailer_generatedtestkey0001')[0];
 
         const expectedKeyInfo = {
@@ -32,5 +64,10 @@ describe('Key.utility - getKeyInfo()', () => {
         expect(keyInfo.username).toEqual(expectedKeyInfo.username);
         expect(keyInfo.email).toEqual(expectedKeyInfo.email);
         expect(keyInfo.website).toEqual(expectedKeyInfo.website);
+    });
+
+    it('Check return false if key not found functionality', async () => {
+        const keyInfo = await keyModule.getKeyInfo('webmailer_generatedtestkey0002');
+        expect(keyInfo).toBe(null);
     });
 });
