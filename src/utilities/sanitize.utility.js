72e3254ea7932ccdefb2822913fe41b81e857e67
DanhLe <d4nhle02@gmail.com>
2024-05-10 21:20:27 -0600

    Sanitize.utility - added SanitizeIp methods to sanitize IP addresses, added unit tests for it in Sanitize.test

--- src/utilities/sanitize.utility.js
+++ src/utilities/sanitize.utility.js
@@ -4,7 +4,7 @@ const { check} = require('express-validator');
 /*
  * Sanitize the key query parameter middleware
  */
-exports.sanitizeKey = [
+const sanitizeKey = [
     check('key').exists().withMessage('Missing required parameter: key').trim().escape(),
 ];
 
@@ -12,21 +12,31 @@ exports.sanitizeKey = [
 /*
  * Sanitize the website_page query parameter middleware
  */
-exports.sanitizeWebsitePage = [
+const sanitizePage = [
     check('page').exists().withMessage('Missing required parameter: page').trim().escape(),
 ];
 
 /*
  * Sanitize the ip query parameter middleware
  */
-exports.sanitizeIp = [
-    check('IP').optional().isIP(4).withMessage('Invalid parameter: IP').trim().escape(),
+const sanitizeIp = [
+    check('IP').isIP(4).withMessage('Invalid parameter: IP').optional().trim().escape(),
 ];
 
 
 /*
  * Sanitize the referer query parameter middleware
  */
-exports.sanitizeReferer = [
-    check('referer').optional().isURL().withMessage('Invalid parameter: referer').trim().escape(),
+const sanitizeReferer = [
+    check('referer').isURL().withMessage('Invalid parameter: referer').optional().customSanitizer(value => {
+      const url = new URL(value);
+      return url.hostname;
+    }),
 ];
+
+exports.sanitizeQuery = [
+    ...sanitizeKey,
+    ...sanitizePage,
+    ...sanitizeIp,
+    ...sanitizeReferer,
+  ];
