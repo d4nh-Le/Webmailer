fd9b59c9504d745030071ee6169db5995ff22f58
DanhLe <d4nhle02@gmail.com>
2024-05-18 03:31:58 -0600

    frontend - fixed design of Intro component

---
+++ frontend/webmailer_front/src/components/register/register.component.js
@@ -0,0 +1,16 @@
+import './register.component.css';
+
+function Register() {
+    return (
+        <div class="register-box">
+            <div class="register-title">Register</div>
+
+
+            <div class="register-content">
+                
+            </div>
+        </div>
+    );
+};
+
+export default Register;
