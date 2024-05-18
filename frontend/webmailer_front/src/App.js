6f0b52f03d3f25a493abc77755e171a55e355f55
Danh Le <danhle002@gmail.com>
2024-05-17 18:00:41 -0600

    frontend - added FooterBar component

--- frontend/webmailer_front/src/App.js
+++ frontend/webmailer_front/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from '../src/components/navbar/navbar.component';
+import FooterBar from './components/footer/footer.component';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <FooterBar />
     </div>
   );
 }
