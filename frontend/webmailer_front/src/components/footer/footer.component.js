4ff8a759233454ad7713ae9c445af524e796ae36
DanhLe <d4nhle02@gmail.com>
2024-05-17 18:00:41 -0600

    frontend - added FooterBar component

---
+++ frontend/webmailer_front/src/components/footer/footer.component.js
@@ -0,0 +1,12 @@
+import './footer.component.css';
+
+function FooterBar() {
+    return (
+       <div class="footer">
+            <div class="footer-note">Testing-build release. For more information or become testers for Webmailer service, please find out more at <a class='footer-link' href='https://github.com/d4nh-Le/webmailer'>Webmailer Github</a>.</div>
+            <div class="footer-credit">@ 2024 Webmailer.</div>
+        </div>
+    );
+}
+
+export  default FooterBar;
