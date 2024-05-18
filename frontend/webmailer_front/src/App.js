65068ea6b2bc1c7a88b50fc69199bc1eb93955d2
DanhLe <d4nhle02@gmail.com>
2024-05-17 19:56:27 -0600

    frontend - added Intro component

--- frontend/webmailer_front/src/App.js
+++ frontend/webmailer_front/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from '../src/components/navbar/navbar.component';
 import FooterBar from './components/footer/footer.component';
+import Intro from './components/intro/intro.component';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
       <FooterBar />
     </div>
   );
