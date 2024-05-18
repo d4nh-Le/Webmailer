de168df9cc5e5b8017577be6a39159c8cd893a0c
DanhLe <d4nhle02@gmail.com>
2024-05-18 03:14:52 -0600

    frontend - added Instruction component and CodeWindow component

--- frontend/webmailer_front/src/App.js
+++ frontend/webmailer_front/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Navbar from '../src/components/navbar/navbar.component';
 import FooterBar from './components/footer/footer.component';
 import Intro from './components/intro/intro.component';
+import Instruction from './components/instruction/instruction.component';
+import CodeWindow from './components/instruction/code_window.component';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <Instruction />
+      <CodeWindow />
       <FooterBar />
     </div>
   );
