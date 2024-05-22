1902daf2d6e67f6e1f8fbba9537a8c5b3e164744
DanhLe <d4nhle02@gmail.com>
2024-05-21 21:09:40 -0600

    Frontend - complete and polish the frontend main page

--- frontend/webmailer_front/src/components/footer/footer.component.js
+++ frontend/webmailer_front/src/components/footer/footer.component.js
@@ -3,8 +3,8 @@ import './footer.component.css';
 function FooterBar() {
     return (
        <div class="footer">
-            <div class="footer-note">Testing-build release. For more information, please visit <a class='footer-link' href='https://github.com/d4nh-Le/webmailer'>Webmailer Github</a>.</div>
-            <div class="footer-credit">@ 2024 Webmailer.</div>
+            <div class="footer-note">Beta release. For more information, please visit <a class='footer-link' href='https://github.com/d4nh-Le/webmailer'>Webmailer Github</a>.</div>
+            <div class="footer-credit">@ 2024 Webmailer - Danh Le.</div>
         </div>
     );
 }
--- frontend/webmailer_front/src/components/register/register.component.js
+++ frontend/webmailer_front/src/components/register/register.component.js
@@ -6,9 +6,17 @@ function Register() {
     const [email, setEmail] = useState('');
     const [page, setPage] = useState('');
     const [username, setUsername] = useState('');
+    const [resNotification, setResNotification] = useState('');
+    const [isError, setIsError] = useState(false);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+
+        if (!email || !page || !username) {
+            setResNotification('Please fill in all fields before submit.');
+            setIsError(true);
+            return;
+        }
 
         const data = {
             email,
@@ -17,8 +25,15 @@ function Register() {
         };
 
         try {
-            const response = await axios.post('http://localhost:8080/registration?email=danhle002@gmail.com&page=test009.com&username=test009');
-            console.log(response.data);
+            const response = await axios.get('http://localhost:8080/registration', { params: data });
+
+            if (response.status === 200) {
+                setResNotification('Successfully registered! Check your email for verification.');
+                setIsError(false);
+            } else {
+                 setResNotification('An error occurred.');
+                 setIsError(true);
+}
         } catch (error) {
             console.error(error);
         }
@@ -26,7 +41,7 @@ function Register() {
 
     return (
         <div className="register-container">
-        <div class="instruction-title">Sign Up</div>
+        <div class="instruction-title" id="register-container-title">Sign Up</div>
 
         <form className="register-form">
             <div className="input-container">
@@ -72,8 +87,12 @@ function Register() {
                 <p>*You will receive a verification message in your email.</p>
             </div>
 
+            <div className={`input-notification ${isError ? 'error' : ''}`}>
+            <p>{resNotification}</p>
+            </div>
+
             <div className="input-submit">
-                <button type="submit" onSubmit={handleSubmit} className="intro-item button get-started">Submit</button>
+                <button type="button" onClick={handleSubmit} className="intro-item button get-started"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 36 36"><path fill="#ffffff" d="M32 13.08v13.63l-7.36-7.36l-1.41 1.41L30.46 28H5.66l7-7.24l-1.44-1.39L4 26.84V9.52l12.43 12.37a2 2 0 0 0 2.82 0l8.83-8.78a7.44 7.44 0 0 1-2-.85l-8.26 8.21L5.31 8h17.5a7.49 7.49 0 0 1-.31-2H4a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h28a2 2 0 0 0 2-2V12.2a7.45 7.45 0 0 1-2 .88" class="clr-i-outline--badged clr-i-outline-path-1--badged"/><circle cx="30" cy="5.86" r="5" fill="#ffffff" class="clr-i-outline--badged clr-i-outline-path-2--badged clr-i-badge"/><path fill="none" d="M0 0h36v36H0z"/></svg>Submit</button>
             </div>
         </form>
         </div>
