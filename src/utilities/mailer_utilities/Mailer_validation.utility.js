eed4ca75a9762f8f612d4a5c043349e63c4990d1
Danh Le <danhle002@gmail.com>
2024-06-18 02:00:58 -0600

    - added script for CI/CD pipeline include: lint, tests, build and deploy, added necessary scripts in package.json

--- src/utilities/mailer_utilities/Mailer_validation.utility.js
+++ src/utilities/mailer_utilities/Mailer_validation.utility.js
@@ -29,7 +29,7 @@ const validateRequestParameters = (req, res, next) => {
  * Validate the key query parameter middleware
  */
 const validateKey = [
-    check('key').exists.isLength({ min: 30, max: 30 }).withMessage('MV203 - Invalid parameter: key - length error'),
+    check('key').isLength({ min: 30, max: 30 }).withMessage('MV203 - Invalid parameter: key - length error'),
     (req, res, next) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
