02018efcbd0526171d528f5cb5a574f77c960936
DanhLe <d4nhle02@gmail.com>
2024-06-24 01:06:35 -0600

    Added reponsive design for navbar component

--- frontend/src/components/navbar/navbar.component.js
+++ frontend/src/components/navbar/navbar.component.js
@@ -1,6 +1,11 @@
 import './navbar.component.css';
+import { useState } from 'react';
 
 function Navbar() {
+
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false); 
+  const toggleDropdown = () => setIsDropdownVisible(!isDropdownVisible);
+
   return (
     <div className="navbar">
 
@@ -13,8 +18,24 @@ function Navbar() {
 
 
         <div class="navbar-right">
+
+        <div class="navright-original">
         <div class="navbar-item"><a href="https://github.com/d4nh-Le/webmailer" target='blank'>Source</a><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="#3c3c43"><path d="M15.64 7.025h-3.622v-2h7v7h-2v-3.55l-4.914 4.914l-1.414-1.414z"/><path d="M10.982 6.975h-6v12h12v-6h-2v4h-8v-8h4z"/></g></svg></div>
-        <div class="navbar-item"><a href="#register-container-title">Sign Up</a></div>  
+        <div class="navbar-item"><a href="#register-container-title">Sign Up</a></div> 
+        </div>
+
+        <div class="navright-dropdown">
+        <div className="dropdown-icon" onClick={toggleDropdown}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 16 16"><path fill="none" stroke="#ff9f39" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5"/></svg>
+        </div>
+
+        {isDropdownVisible && (
+        <div className="dropdown-menu show">
+         <div class="navbar-item"><a href="https://github.com/d4nh-Le/webmailer" target='blank'>Source</a><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="#3c3c43"><path d="M15.64 7.025h-3.622v-2h7v7h-2v-3.55l-4.914 4.914l-1.414-1.414z"/><path d="M10.982 6.975h-6v12h12v-6h-2v4h-8v-8h4z"/></g></svg></div>
+         <div class="navbar-item"><a href="#register-container-title">Sign Up</a></div> 
+        </div>
+        )}
+        </div>
         </div>
 
     </div>
