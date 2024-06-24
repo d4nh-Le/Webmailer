1de8413c27b9f5d111ff438485405230a0a89552
Danh Le <danhle002@gmail.com>
2024-06-24 01:21:19 -0600

    added responsive design for intro component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,8 +15,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Intro />
-      <BriefMap />
+      <Intro />
+      {/* <BriefMap />
       <Instruction />
       <CodeWindow />
       <Register />
